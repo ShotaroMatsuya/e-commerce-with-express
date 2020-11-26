e08139fa33f8a597c65bb6d85192a185aa92a763
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-11-26 15:25:43 +0900

    gitignore now working

--- controllers/admin.js
+++ controllers/admin.js
@@ -70,14 +70,18 @@ exports.postEditProduct = (req,res,next)=>{
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
     Product.findById(prodId).then(product=>{
+      if(product.userId.toString() !== req.user._id.toString()){//product作成者以外は編集できないようにする
+        return res.redirect('/');
+      }
+
       product.title =updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
       product.imageUrl = updatedImageUrl;
-      return product.save();
-    }).then(result => {
-      console.log('UPDATED PRODUCT!');
-      res.redirect('/admin/products');
+      return product.save().then(result => {
+        console.log('UPDATED PRODUCT!');
+        res.redirect('/admin/products');
+      });
     })
     .catch(err => console.log(err));
     // const product = new Product(
@@ -90,7 +94,7 @@ exports.postEditProduct = (req,res,next)=>{
 
 }
 exports.getProducts = (req,res,next)=>{
-    Product.find()
+  Product.find({userId:req.user._id})//adminでは自分が作成したproductsのみを表示
     // .select('title price - _id')//抽出するProductモデルのフィールドを指定(titleとpriceを取得しidを除くという意味)
     // .populate('userId','name')//productモデルのuserIdからuserモデルを抽出し、さらにuserモデルのnameのみを抽出するという意味
     .then(products => {
@@ -104,7 +108,7 @@ exports.getProducts = (req,res,next)=>{
 }
 exports.postDeleteProduct =(req,res,next)=>{
     const prodId = req.body.productId;
-    Product.findByIdAndRemove(prodId)
+    Product.deleteOne({_id:prodId,userId:req.user._id})
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
